b82730516357344b0a771f965b6d912643ec4203
Diego Rodriguez <diegomanners2000@gmail.com>
2023-04-03 19:02:30 -0500

    Tap Limit

--- Assets/_Scripts/PlaceObject.cs
+++ Assets/_Scripts/PlaceObject.cs
@@ -15,39 +15,62 @@ public class PlaceObject : MonoBehaviour
     private ARRaycastManager aRRaycastManager;
     private ARPlaneManager aRPlaneManager;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    public int maxTaps = 1;
+    private int tapCount = 0;
 
     private void Awake()
     {
         aRRaycastManager = GetComponent<ARRaycastManager>();
         aRPlaneManager = GetComponent<ARPlaneManager>();
     }
-    
-    private void OnEnable()
-    {
-        EnhancedTouch.TouchSimulation.Enable();
-        EnhancedTouch.EnhancedTouchSupport.Enable();
-        EnhancedTouch.Touch.onFingerDown += FingerDown;
-    }
-    
-    private void OnDisable()
-    {
-        EnhancedTouch.TouchSimulation.Disable();
-        EnhancedTouch.EnhancedTouchSupport.Disable();
-        EnhancedTouch.Touch.onFingerDown -= FingerDown;
-    }
 
-    private void FingerDown(EnhancedTouch.Finger finger)
-    {
-        if (finger.index != 0) return;
+    //private void OnEnable()
+    //{
+    //    EnhancedTouch.TouchSimulation.Enable();
+    //    EnhancedTouch.EnhancedTouchSupport.Enable();
+    //    EnhancedTouch.Touch.onFingerDown += FingerDown;
+    //}
 
-        if (aRRaycastManager.Raycast(finger.currentTouch.screenPosition, hits, TrackableType.PlaneWithinPolygon)){
-            foreach(ARRaycastHit hit in hits)
+    //private void OnDisable()
+    //{
+    //    EnhancedTouch.TouchSimulation.Disable();
+    //    EnhancedTouch.EnhancedTouchSupport.Disable();
+    //    EnhancedTouch.Touch.onFingerDown -= FingerDown;
+    //}
+
+    //private void FingerDown(EnhancedTouch.Finger finger)
+    //{
+    //    if (finger.index != 0) return;
+
+    //    if (aRRaycastManager.Raycast(finger.currentTouch.screenPosition, hits, TrackableType.PlaneWithinPolygon)){
+    //        foreach(ARRaycastHit hit in hits)
+    //        {
+    //            Pose pose = hit.pose;
+    //            GameObject obj = Instantiate(prefab, pose.position, pose.rotation);
+    //        }
+
+    //    }
+
+    //}
+    private void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
             {
-                Pose pose = hit.pose;
-                GameObject obj = Instantiate(prefab, pose.position, pose.rotation);
-            }
+                if (aRRaycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = hits[0].pose;
 
+                    if (tapCount < maxTaps)
+                    {
+                        Instantiate(prefab, hitPose.position, hitPose.rotation);
+                        tapCount++;
+                    }
+                }
+            }
         }
-      
     }
 }
